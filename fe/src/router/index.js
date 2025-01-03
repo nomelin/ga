09d9233a295a4bcd6dc2f4c68fef789ac39a62ad
nomelin <2023686595@qq.com>
2025-01-03 22:00:46 +0800

    前后端脚手架

---
+++ fe/src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+// 解决导航栏或者底部导航tabBar中的vue-router在3.0版本以上频繁点击菜单报错的问题。
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err)
+}
+
+const routes = [
+    {
+        path: '/front',
+        name: '首页',
+        component: () => import('../views/Front.vue'),
+    },
+    {path: '*', name: 'NotFound', meta: {name: '无法访问'}, component: () => import('../views/404.vue')},
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.path === '/' || to.path === '') {
+        next('/front')
+
+    } else {
+        next()
+    }
+})
+
+export default router
---
+++ fe/src/views/Front.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div style="height: 100vh; overflow: hidden; display: flex; align-items: center; justify-content: center">
+      <div style="font-size: 40px">主页</div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "front",
+  data() {
+    return {}
+  },
+  created() {
+
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
