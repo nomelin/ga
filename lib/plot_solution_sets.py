cf25c46b84ee01ec9465b13bbbe5a696832843dd
”xhgyj“ <2697866598@qq.com>
2025-01-06 15:53:58 +0800

    画图更新保存gif

--- lib/plot_solution_sets.py
+++ lib/plot_solution_sets.py
@@ -3,13 +3,14 @@ import matplotlib.animation as animation
 import os
 
 
-def plot_solution_sets(current_solutions, predicted_solutions):
+def plot_solution_sets(current_solutions, predicted_solutions, file_name="solution_sets.gif"):
     """
     绘制两个解集的可视化图，并将其保存为GIF格式。
 
     参数:
     - current_solutions (list of list): 当前解集，每个解是一个列表 [x1, x2, ..., xn]。
     - predicted_solutions (list of list): 预测解集，每个解是一个列表 [x1, x2, ..., xn]。
+    - file_name (str): 保存的GIF文件名，默认为 "solution_sets.gif"。
     """
     # 获取当前目录的上级目录
     parent_dir = os.path.dirname(os.getcwd())
@@ -55,7 +56,7 @@ def plot_solution_sets(current_solutions, predicted_solutions):
     ani = animation.FuncAnimation(fig, update, frames=len(predicted_solutions), repeat=False)
 
     # 保存为GIF（路径为当前目录的上级目录中的image文件夹）
-    gif_path = os.path.join(image_dir, 'solution_sets.gif')
+    gif_path = os.path.join(image_dir, file_name)
     ani.save(gif_path, writer='pillow', fps=2)
 
     plt.show()
@@ -64,4 +65,7 @@ def plot_solution_sets(current_solutions, predicted_solutions):
 # 示例用法
 current_solutions = [[1, 2], [3, 1], [2, 3], [4, 2]]
 predicted_solutions = [[1.2, 2.1], [3.1, 0.9], [2.2, 3.2], [4.1, 2.2]]
-plot_solution_sets(current_solutions, predicted_solutions)
+
+
+# 传入自定义文件名
+plot_solution_sets(current_solutions, predicted_solutions, "custom_solution_sets.gif")
