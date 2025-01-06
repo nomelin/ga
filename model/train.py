5c7d2999745382303dd4d44bc353861d08cb0538
nomelin <2023686595@qq.com>
2025-01-06 14:41:26 +0800

    模型

--- model/train.py
+++ model/train.py
@@ -2,6 +2,9 @@ import json
 
 import torch
 
+from model.PopulationPredictorLSTM import PopulationPredictorLSTM
+from model.loss import calculate_loss
+
 
 def load_population_data(json_file):
     """
@@ -23,6 +26,58 @@ def load_population_data(json_file):
         samples.append((inputs, target))
     return samples
 
+
+def train_model_no_batch(model, samples, epochs, alpha=1.0, beta=0.1, lr=1e-3, device='cpu'):
+    """
+    不使用批量处理的 LSTM 模型训练函数。
+    参数:
+        model (nn.Module): 预测模型。
+        samples (list): 数据样本列表，每个样本包含输入和目标种群。
+        epochs (int): 训练轮数。
+        alpha (float): 匹配损失权重。
+        beta (float): 多样性损失权重。
+        lr (float): 学习率。
+        device (str): 运行设备（'cpu' 或 'cuda'）。
+    """
+    model.to(device)
+    print(f"Training on {device}...")
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    for epoch in range(epochs):
+        total_loss = 0.0
+
+        for sample in samples:
+            inputs, target = sample
+            inputs, target = inputs.to(device), target.to(device)  # 添加 batch 维度
+            # print(f"size of inputs: {inputs.size()}, size of target: {target.size()}")
+
+            optimizer.zero_grad()
+
+            # 前向传播
+            predicted = model(inputs)
+            # print(f"size of predicted: {predicted.size()}")
+
+            # 计算损失
+            loss = calculate_loss(predicted, target, alpha, beta)
+
+            # 反向传播和优化
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+
+        print(f"----- Epoch {epoch + 1}/{epochs}, Loss: {total_loss / len(samples):.4f} -----")
+
+
 if __name__ == '__main__':
-    # 测试 load_population_data 函数
-    json_file = 'data/population.json'
+    json_file = '../data/optimal_solutions_dy1.json'
+    epochs = 50
+    learning_rate = 1e-3
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # 加载数据
+    samples = load_population_data(json_file)
+
+    # 初始化模型
+    model = PopulationPredictorLSTM()
+    train_model_no_batch(model, samples, epochs, lr=learning_rate, device=device)
